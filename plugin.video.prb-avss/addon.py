4299485fb27c7bbf2d53afd9c9866969bbc5ed38
Phantom Raspberry Blower <jasonbulis@hotmail.com>
2021-10-11 16:37:21 +0100

    Updated PRB-AVSS

--- plugin.video.prb-avss/addon.py
+++ plugin.video.prb-avss/addon.py
@@ -2,4 +2,4 @@
 import xbmc
 
 if __name__ == '__main__':
-    xbmc.executebuiltin('XBMC.PlayMedia("udp://224.0.0.1:4569")', True)
+    xbmc.executebuiltin('PlayMedia("udp://224.0.0.1:4569")', True)
